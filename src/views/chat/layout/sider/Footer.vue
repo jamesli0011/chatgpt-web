2e5804c15ad211264925373df8c90c050799eee3
james <james@lightpaw.com>
2023-05-14 17:03:10 +0800

    update

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,10 +1,5 @@
 <script setup lang='ts'>
-import { defineAsyncComponent, ref } from 'vue'
-import { HoverButton, SvgIcon, UserAvatar } from '@/components/common'
 
-const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
-
-const show = ref(false)
 </script>
 
 <template>
